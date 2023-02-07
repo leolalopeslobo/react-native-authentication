0aa1b41522bfe3e99cae0d7790dc3b0142eea50f
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2023-02-07 07:06:24 +0530

    phone authentication

---
+++ phone-authentication/src/index.js
@@ -0,0 +1,118 @@
+import { TouchableOpacity, Text, View, TextInput, StyleSheet, Alert } from "react-native";
+import React, { useRef, useState } from "react";
+import { FirebaseRecaptchaVerifierModal } from "expo-firebase-recaptcha"; 'expo-firebase-recaptcha';
+import { firebaseConfig } from "../config";
+import firebase from "firebase/compat/app";
+import Home from "../screens/home";
+
+const Otp = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [code, setCode] = useState('');
+    const [verificationId, setVerificationId] = useState(null);
+    const recaptchaVerifier = useRef(null);
+
+    const sendVerification = () => {
+        const phoneProvider = new firebase.auth.PhoneAuthProvider();
+        phoneProvider
+            .verifyPhoneNumber(phoneNumber, recaptchaVerifier.current)
+            .then(setVerificationId);
+            setPhoneNumber('');
+    }; 
+    const confirmCode = () => {
+        const credential = firebase.auth.PhoneAuthProvider.credential(
+            verificationId,
+            code
+        );
+        firebase.auth().signInWithCredential(credential)
+        .then(() => {
+            setCode('');
+        })
+        .catch((error) => {
+            //show an alert in case of error
+            alert(error);
+        })
+        Alert.alert(
+            'Login Successful. Welcome to HomeScreen',
+            // <Home />
+        );
+    }
+
+    return (
+        <View style={StyleSheet.container}>
+            <FirebaseRecaptchaVerifierModal
+            ref={recaptchaVerifier}
+            firebaseConfig={firebaseConfig}
+            />
+            <Text style={styles.otpText}>
+                Login using OTP
+            </Text>
+            <TextInput
+                placeholder="Phone Number with country code"
+                onChangeText={setPhoneNumber}
+                keyboardType='phone-pad'
+                autoCompleteType='tel'
+                style={styles.textInput}
+                />
+                <TouchableOpacity style={styles.sendVerification} onPress={sendVerification}>
+                    <Text style={styles.buttonText}>
+                        Send Verification
+                    </Text>
+                </TouchableOpacity>
+                <TextInput
+                placeholder="confirm code"
+                onChangeText={setCode}
+                keyboardType='number-pad'
+                style={styles.textInput}
+                />
+                <TouchableOpacity style={styles.sendCode} onPress={confirmCode}>
+                    <Text style={styles.buttonText}>
+                        Confirm Verification
+                    </Text>
+                </TouchableOpacity>
+                
+        </View>
+    )
+}
+
+export default Otp
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#000',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    textInput:{
+        paddingTop:40,
+        paddingBottom:20,
+        paddingHorizontal:20,
+        fontSize:24,
+        borderBottomColor:'#fff',
+        borderBottomWidth:2,
+        marginBottom:20,
+        textAlign:'center',
+        color:'#000'
+    },
+    sendVerification:{
+        padding:20,
+        backgroundColor:'#00495E',
+        borderRadius:10
+    },
+    sendCode:{
+        padding:20,
+        backgroundColor:'#00495E',
+        borderRadius:10
+    },
+    buttonText:{
+        textAlign:'center',
+        color:'#fff',
+        fontWeight:'bold',
+    },
+    otpText:{
+        fontSize:24,
+        fontWeight:'bold',
+        color:'#fff',
+        margin:20
+    }
+})
